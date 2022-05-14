4f18e9b56928b9f6a1949e3f8ec8ed94a845efff
Ali Punjabi <raja.ehtisham222@gmail.com>
2022-05-14 10:23:09 +0500

    Description box integrated

--- lib/Screens/addpropertyscreen.dart
+++ lib/Screens/addpropertyscreen.dart
@@ -23,6 +23,21 @@ class _addPropertyScreenState extends State<addPropertyScreen> {
   String? dropdownValue3;
   String? dropdownValue4;
   String? dropdownValue5;
+  bool bold = false;
+
+  bool italic = false;
+
+  bool underline = false;
+
+  bool align = false;
+
+  bool centerAlign = false;
+
+  bool leftAlign = false;
+
+  bool rightAlign = false;
+
+  bool color = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -664,9 +679,132 @@ class _addPropertyScreenState extends State<addPropertyScreen> {
                   textEditingController: _textprice,
                 ),
               ),
-              SizedBox(
-                height: 240,
-              )
+              Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Center(
+                  child: Container(
+                    height: 170,
+                    padding: const EdgeInsets.all(10),
+                    decoration: BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.circular(10.0),
+                      border: Border.all(color: Colors.grey.withOpacity(0.2)),
+                    ),
+                    child: Column(
+                      children: [
+                        TextFormField(
+                          keyboardType: TextInputType.text,
+                          textAlign: align
+                              ? TextAlign.justify
+                              : centerAlign
+                                  ? TextAlign.center
+                                  : leftAlign
+                                      ? TextAlign.left
+                                      : rightAlign
+                                          ? TextAlign.right
+                                          : TextAlign.start,
+                          decoration: InputDecoration(
+                            hintText: 'Description',
+                            errorStyle: TextStyle(color: Colors.red),
+                            border: InputBorder.none,
+                            contentPadding: EdgeInsets.zero,
+                          ),
+                          maxLines: 6,
+                          style: TextStyle(
+                            decoration:
+                                underline ? TextDecoration.underline : null,
+                            fontSize: 16,
+                            color: color ? Colors.blue : Colors.black,
+                            fontWeight: bold ? FontWeight.bold : null,
+                            fontStyle:
+                                italic ? FontStyle.italic : FontStyle.normal,
+                          ),
+                        ),
+                        Padding(
+                          padding: const EdgeInsets.only(top: 8.0),
+                          child: Container(
+                            color: Color.fromARGB(255, 203, 197, 197),
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                              children: [
+                                GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        bold = bold ? false : true;
+                                      });
+                                    },
+                                    child: Icon(Icons.format_bold)),
+                                GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        italic = italic ? false : true;
+                                      });
+                                    },
+                                    child: Icon(Icons.format_italic)),
+                                GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        underline = underline ? false : true;
+                                      });
+                                    },
+                                    child: Icon(Icons.format_underline)),
+                                GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        align = align ? false : true;
+                                        centerAlign = false;
+                                        leftAlign = false;
+                                        rightAlign = false;
+                                      });
+                                    },
+                                    child: Icon(Icons.menu)),
+                                GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        centerAlign =
+                                            centerAlign ? false : true;
+                                        align = false;
+                                        leftAlign = false;
+                                        rightAlign = false;
+                                      });
+                                    },
+                                    child: Icon(Icons.filter_list)),
+                                GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        leftAlign = leftAlign ? false : true;
+                                        align = false;
+                                        centerAlign = false;
+                                        rightAlign = false;
+                                      });
+                                    },
+                                    child: Icon(Icons.sort)),
+                                GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        rightAlign = rightAlign ? false : true;
+                                        align = false;
+                                        centerAlign = false;
+                                        leftAlign = false;
+                                      });
+                                    },
+                                    child: Icon(Icons.segment)),
+                                GestureDetector(
+                                    onTap: () {
+                                      setState(() {
+                                        color = color ? false : true;
+                                      });
+                                    },
+                                    child: Icon(Icons.format_color_fill))
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
             ],
           ),
 
