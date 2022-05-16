796a2c18b748e939f36a2543937280afedcdb004
Ali Punjabi <raja.ehtisham222@gmail.com>
2022-05-16 10:32:00 +0500

    Appbar back button on contact us screen

--- lib/Screens/contactusscreen.dart
+++ lib/Screens/contactusscreen.dart
@@ -10,94 +10,103 @@ class ContactScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
-          children: [
-            SizedBox(
-              height: 100,
-            ),
-            Text(
-              'Need Some Help?',
-              style: TextStyle(
-                  color: Colors.black,
-                  fontWeight: FontWeight.bold,
-                  fontSize: 18),
-            ),
-            SizedBox(
-              height: 20,
-            ),
-            Text(
-              'Reach out to us with your issue, question or feedback\nWe Love Hearing From You',
-              style: TextStyle(color: Colors.grey),
-            ),
-            SizedBox(
-              height: 100,
-            ),
-            Container(
-                decoration: BoxDecoration(
-                    color: Color.fromARGB(255, 244, 239, 239),
-                    borderRadius: BorderRadius.circular(10)),
-                child: Padding(
-                  padding: const EdgeInsets.fromLTRB(12.0, 24, 24, 24),
-                  child: Column(children: [
-                    CustomTextField(
-                      onFieldSubmitted: (v) {
-                        FocusScope.of(context).nextFocus();
-                      },
-                      textInputAction: TextInputAction.next,
-                      hintText: 'Name',
-                      textInputType: TextInputType.emailAddress,
-                      textEditingController: _textname,
-                    ),
-                    const SizedBox(height: 15),
-                    CustomTextField(
-                      onFieldSubmitted: (v) {
-                        FocusScope.of(context).nextFocus();
-                      },
-                      textInputAction: TextInputAction.next,
-                      hintText: 'Email',
-                      textInputType: TextInputType.emailAddress,
-                      textEditingController: _textemail,
-                      validator: validateEmail,
-                    ),
-                    const SizedBox(height: 15),
-                    CustomTextField(
-                      onFieldSubmitted: (v) {
-                        FocusScope.of(context).nextFocus();
-                      },
-                      textInputAction: TextInputAction.next,
-                      hintText: 'Phone',
-                      textInputType: TextInputType.emailAddress,
-                      textEditingController: _textphone,
-                      validator: validateMobile,
-                    ),
-                    const SizedBox(height: 25),
-                    TextField(
-                      maxLines: 5,
-                      expands: false,
-                      decoration: InputDecoration(
-                        border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(8.0),
-                          borderSide:
-                              const BorderSide(width: 0.5, color: Colors.grey),
-                        ),
-                        hintText: "Message",
-                        hintStyle: TextStyle(color: Colors.grey),
-                        filled: true,
-                        fillColor: Constants.colorSecond,
+      appBar: AppBar(
+        iconTheme: IconThemeData(
+          color: Constants.colorMain, //change your color here
+        ),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              SizedBox(
+                height: 50,
+              ),
+              Text(
+                'Need Some Help?',
+                style: TextStyle(
+                    color: Colors.black,
+                    fontWeight: FontWeight.bold,
+                    fontSize: 18),
+              ),
+              SizedBox(
+                height: 20,
+              ),
+              Text(
+                'Reach out to us with your issue, question or feedback\nWe Love Hearing From You',
+                style: TextStyle(color: Colors.grey),
+              ),
+              SizedBox(
+                height: 100,
+              ),
+              Container(
+                  decoration: BoxDecoration(
+                      color: Color.fromARGB(255, 244, 239, 239),
+                      borderRadius: BorderRadius.circular(10)),
+                  child: Padding(
+                    padding: const EdgeInsets.fromLTRB(12.0, 24, 24, 24),
+                    child: Column(children: [
+                      CustomTextField(
+                        onFieldSubmitted: (v) {
+                          FocusScope.of(context).nextFocus();
+                        },
+                        textInputAction: TextInputAction.next,
+                        hintText: 'Name',
+                        textInputType: TextInputType.emailAddress,
+                        textEditingController: _textname,
                       ),
-                      keyboardType: TextInputType.multiline,
-                    ),
-                    const SizedBox(height: 15),
-                    SizedBox(
-                        width: 330,
-                        child: CustomElevatedButton(
-                            onPressed: (() {}), child: Text('Send')))
-                  ]),
-                )),
-          ],
+                      const SizedBox(height: 15),
+                      CustomTextField(
+                        onFieldSubmitted: (v) {
+                          FocusScope.of(context).nextFocus();
+                        },
+                        textInputAction: TextInputAction.next,
+                        hintText: 'Email',
+                        textInputType: TextInputType.emailAddress,
+                        textEditingController: _textemail,
+                        validator: validateEmail,
+                      ),
+                      const SizedBox(height: 15),
+                      CustomTextField(
+                        onFieldSubmitted: (v) {
+                          FocusScope.of(context).nextFocus();
+                        },
+                        textInputAction: TextInputAction.next,
+                        hintText: 'Phone',
+                        textInputType: TextInputType.emailAddress,
+                        textEditingController: _textphone,
+                        validator: validateMobile,
+                      ),
+                      const SizedBox(height: 25),
+                      TextField(
+                        maxLines: 5,
+                        expands: false,
+                        decoration: InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(8.0),
+                            borderSide: const BorderSide(
+                                width: 0.5, color: Colors.grey),
+                          ),
+                          hintText: "Message",
+                          hintStyle: TextStyle(color: Colors.grey),
+                          filled: true,
+                          fillColor: Constants.colorSecond,
+                        ),
+                        keyboardType: TextInputType.multiline,
+                      ),
+                      const SizedBox(height: 15),
+                      SizedBox(
+                          width: 330,
+                          child: CustomElevatedButton(
+                              onPressed: (() {}), child: Text('Send')))
+                    ]),
+                  )),
+            ],
+          ),
         ),
       ),
     );
