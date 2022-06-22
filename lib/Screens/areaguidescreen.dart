49eddb27368ad4547f6a69693b2ee1742a697d9d
Ali Punjabi <raja.ehtisham222@gmail.com>
2022-06-22 14:45:56 +0500

    area guide screen issue resolved

--- lib/Screens/areaguidescreen.dart
+++ lib/Screens/areaguidescreen.dart
@@ -1,4 +1,4 @@
-// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors
+// ignore_for_file: prefer_const_literals_to_create_immutables, prefer_const_constructors, dead_code
 
 import 'package:arzenafees/Components/Export/custom_import.dart';
 import 'package:arzenafees/model/areaview.dart';
@@ -16,14 +16,21 @@ class areaguide extends StatelessWidget {
         body: FutureBuilder(
       future: fetchareaguide(areaName, areaSubtype),
       builder: (context, AsyncSnapshot snapshot) {
-        if (!snapshot.hasData) {
+        if (!snapshot.hasData && !snapshot.hasError) {
           return Center(child: CircularProgressIndicator());
+        }
+        if (snapshot.hasError) {
+          return Center(
+            child: Text("No data Found"),
+          );
         } else {
-          final guide = (snapshot.data)['areaGuide'];
-          final String sector = guide[0]['sector'];
+          final Map<String, dynamic> guide = (snapshot.data)['areaGuide'];
+          final String sector = guide['sector'];
           final String city = guide['city'];
           final String description = guide['description'];
-          return guide;
+          return Center(
+            child: Text(city),
+          );
         }
       },
     ));
--- lib/services/areaguideapi.dart
+++ lib/services/areaguideapi.dart
@@ -7,12 +7,15 @@ import 'package:http/http.dart' as http;
 List<AreaGuide>? areaguide;
 Future<void> fetchareaguide(String areaName, String areaSubtype) async {
   final response = await http.get(Uri.parse(
-      'https://arz-e-nafees.nafeessolutions.com/public/api/ApiAreaGuide?area=$areaSubtype&city=$areaName'));
-  if (response.statusCode == 200) {
+      'https://arz-e-nafees.nafeessolutions.com/public/api/ApiAreaGuide?area=G-13&city=Islamabad'));
+  if (response.statusCode == 200 || response.statusCode == 500) {
     var guide = (json.decode(response.body));
-    Areaguide guidearea = Areaguide.fromJson(guide);
-    areaguide = guidearea.areaGuide;
+    // Areaguide guidearea = Areaguide.fromJson(guide);
+    //areaguide = guidearea.areaGuide;
     return guide;
+  }
+  if (response.statusCode == 500) {
+    throw Exception('No Data Found!');
   } else {
     throw Exception('Unexpected error occured!');
   }
